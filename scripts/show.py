804d411896ece1d06f5dc2ebab35ad1972d3ba74
Simon Kokkendorff <silyko@gmail.com>
2013-10-22 12:42:55 +0200

    minor change

--- scripts/show.py
+++ scripts/show.py
@@ -7,8 +7,9 @@ import matplotlib
 matplotlib.use("Qt4Agg")
 import matplotlib.pyplot as plt
 pc=pointcloud.fromLAS(sys.argv[1])
+pc=pc.cut_to_z_interval(-20,200)
 pc.triangulate()
-g=pc.get_grid(800,800,crop=10)
+g=pc.get_grid(800,800,crop=10,nd_val=-30)
 bbox=g.get_bounds()
 im=plt.imshow(g.grid,extent=(bbox[0],bbox[2],bbox[3],bbox[1]))
 plt.colorbar(im)
