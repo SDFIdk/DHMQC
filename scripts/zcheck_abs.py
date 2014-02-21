b600e6d8ac3bc51f35d13ed1c353f00b540ad595
Simon Kokkendorff <silyko@gmail.com>
2014-02-21 11:16:51 +0100

    zcheck_abs now works for line segments as well.

--- scripts/zcheck_abs.py
+++ scripts/zcheck_abs.py
@@ -15,14 +15,21 @@ xy_tolerance=2.0
 #z_tolerance
 z_tolerance=1.0
 #The class(es) we want to look at...
-cut_class=[dhmqc_constants.terrain,dhmqc_constants.surface]
+cut_class=dhmqc_constants.terrain
+#The z-interval we want to consider
+Z_MIN=-20
+Z_MAX=200
 
 def usage():
-	print("Call:\n%s <las_file> <point_datasource> [-text] [-delim <delim>] [-use_local] [-ftype <type>]" %os.path.basename(sys.argv[0]))
-	print("If -text is NOT specified, it is assumed that the datasource is an OGR readable point data source.")
+	print("Call:\n%s <las_file> <point_datasource> (options)" %os.path.basename(sys.argv[0]))
+	print("Options:")
+	print("[-text] [-delim <delim>] [-use_local] [-ftype <type>] [-toE]")
+	print("If -text is NOT specified, it is assumed that the datasource is an OGR readable data source.")
+	print("Input reference geometries can be 3D points or 3D line features.")
 	print("If -text IS specified, -delim <delim> can be used to specify delimiter character(s).")
 	print("Use -use_local to force use of local database for reporting.")
-	print("Use -ftype <type> to specify a 'feature' type of points source (patch, road etc.)")
+	print("Use -ftype <type> to specify a 'feature' type of reference points  (patch, road etc.)")
+	print("Use -toE to transform input points from dvr90 to ellipsoidal heights (TODO).")
 	sys.exit()
 
 
@@ -44,42 +51,7 @@ def check_points(pc,xy,z,DEBUG=False):
 	return m,sd,n #consider using also l1....
 
 
-def main(args):
-	if len(args)<3:
-		usage()
-	#standard dhmqc idioms....#
-	lasname=args[1]
-	pointname=args[2]
-	kmname=get_1km_name(lasname)
-	print("Running %s on block: %s, %s" %(os.path.basename(args[0]),kmname,time.asctime()))
-	use_local="-use_local" in args
-	if use_local:
-		print("Using local data source for reporting.")
-	else:
-		print("Using global data source for reporting.")
-	ds_report=report.get_output_datasource(use_local)
-	if ds_report is None:
-		print("Failed to open report datasource - you might need to CREATE one...")
-	pc=pointcloud.fromLAS(lasname).cut_to_z_interval(-20,200).cut_to_class(cut_class) #what to cut to here...??
-	if "-text" in args:
-		delim=None
-		if "-delim" in args:
-			i=args.index("-delim")
-			delim=args[i+1]
-		points_arr=np.loadtxt(pointname,delimiter=delim)
-	else:
-		points=vector_io.get_geometries(pointname)
-		points_arr=array_geometry.ogrpoints2array(points)
-		del points  #not needed anymore...
-	if points_arr.ndim==1:
-		points_arr=points_arr.reshape((1,2))
-	if "-ftype" in args:
-		i=args.index("-ftype")
-		ftype=args[i+1]
-	else:
-		ftype=None
-	xy=array_factory.point_factory(points_arr[:,:2])
-	z=array_factory.z_factory(points_arr[:,2])
+def do_it(xy,z,km_name="",ftype="NA",ds_report=None):
 	bbox=array_geometry.get_bounds(xy)
 	bbox_poly=array_geometry.bbox_to_polygon(bbox) #perhaps use that for intersection to speed up??
 	#calulate center of mass for reporting...
@@ -109,6 +81,99 @@ def main(args):
 		#what geometry should be reported, bounding box??
 		if ds_report is not None:
 			report.report_abs_z_check(ds_report,kmname,m,sd,n,id,ftype,ogr_geom=cm_geom)
+
+def main(args):
+	if len(args)<3:
+		usage()
+	#standard dhmqc idioms....#
+	lasname=args[1]
+	pointname=args[2]
+	kmname=get_1km_name(lasname)
+	print("Running %s on block: %s, %s" %(os.path.basename(args[0]),kmname,time.asctime()))
+	use_local="-use_local" in args
+	if use_local:
+		print("Using local data source for reporting.")
+	else:
+		print("Using global data source for reporting.")
+	ds_report=report.get_output_datasource(use_local)
+	if ds_report is None:
+		print("Failed to open report datasource - you might need to CREATE one...")
+	pc=pointcloud.fromLAS(lasname).cut_to_z_interval(Z_MIN,Z_MAX).cut_to_class(cut_class) #what to cut to here...??
+	if "-ftype" in args:
+		i=args.index("-ftype")
+		ftype=args[i+1]
+	else:
+		ftype=None
+	features=[]
+	bboxes=[]
+	cm_geoms=[]
+	#Load the point sets into a list of 'patches'
+	if "-text" in args:
+		delim=None
+		if "-delim" in args:
+			i=args.index("-delim")
+			delim=args[i+1]
+		points_arr=np.loadtxt(pointname,delimiter=delim)
+		if points_arr.ndim==1:
+			points_arr=points_arr.reshape((1,3))
+		features.append(points_arr)
+	else:
+		geoms=vector_io.get_geometries(pointname)
+		#test geometry dimension
+		if len(geoms)>0 and geoms[0].GetDimension()>0:
+			for geom in geoms:
+				features.append(array_geometry.ogrline2array(geom,flatten=False))
+		else:
+			points_arr=array_geometry.ogrpoints2array(points)
+			if points_arr.ndim==1:
+				points_arr=points_arr.reshape((1,3))
+			features.append(points_arr)
+	print("Checking %d point sets" %len(features))
+	#Prepare data to proper format...(xy,z) etc....
+	prepared_points=[]
+	for arr in features:
+		xy=array_factory.point_factory(arr[:,:2])
+		z=array_factory.z_factory(arr[:,2])
+		prepared_points.append((xy,z))
+		bbox=array_geometry.get_bounds(xy)
+		#calulate center of mass for reporting...
+		cm_x,cm_y=xy.mean(axis=0)
+		cm_z=z.mean()
+		print("Center of mass: %.2f %.2f %.2f" %(cm_x,cm_y,cm_z))
+		cm_geom=ogr.Geometry(ogr.wkbPoint25D)
+		cm_geom.SetPoint(0,cm_x,cm_y,cm_z)
+		bboxes.append(bbox)
+		cm_geoms.append(cm_geom)
+	#Loop over strips#
+	for id in pc.get_pids():
+		print("%s\n" %("+"*70))
+		print("Strip id: %d" %id)
+		pc_=pc.cut_to_strip(id)
+		if pc_.get_size()<50:
+			print("Not enough points...")
+			continue
+		might_intersect=[]
+		for i,bbox in enumerate(bboxes):
+			if pc_.might_intersect_box(bbox):
+				might_intersect.append(i)
+		if len(might_intersect)==0:
+			print("Strip does not intersect any point 'patch'...")
+			continue
+		pc_.triangulate()
+		pc_.calculate_validity_mask(angle_tolerance,xy_tolerance,z_tolerance)
+		#now loop over the patches which might intersect this strip...
+		for i in might_intersect:
+			xy,z=prepared_points[i]
+			cm_geom=cm_geoms[i]
+			print("Stats for check of 'patch/set' %d against strip %d:" %(i,id))
+			stats=check_points(pc_,xy,z)
+			if stats is None:
+				print("Not enough points in proper triangles...")
+				continue
+			m,sd,n=stats
+			#what geometry should be reported, bounding box??
+			if ds_report is not None:
+				report.report_abs_z_check(ds_report,kmname,m,sd,n,id,ftype,ogr_geom=cm_geom)
 
 
 if __name__=="__main__":
