62a82529c8fdfd73736c2ebc12a6e0ab602715eb
Simon Kokkendorff <silyko@gmail.com>
2014-03-19 13:34:31 +0100

    -F -> -S in page reflected in density_check

--- scripts/density_check.py
+++ scripts/density_check.py
@@ -13,7 +13,7 @@ if DEBUG:
 	matplotlib.use("Qt4Agg")
 	import matplotlib.pyplot as plt
 #-b decimin signals that returnval is min_density*10, -p
-PAGE_ARGS=[os.path.join("lib","page"),"-F","Rlast"]
+PAGE_ARGS=[os.path.join("lib","page"),"-S","Rlast"]
 PAGE_BOXDEN_SWITCH="-p"
 PAGE_BOXDEN_FRMT="boxdensity:{0:.0f}"
 PAGE_GRID_FRMT="G/{0:.2f}/{1:.2f}/{2:.0f}/{3:.0f}/{4:.4f}/-9999"
