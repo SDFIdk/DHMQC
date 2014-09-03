7c33e25a6cef964571e26f5ec1fda4846b756a50
Simon Kokkendorff <silyko@gmail.com>
2014-09-03 14:21:05 +0200

    z_precision_roads updated..

--- qc/z_precision_roads.py
+++ qc/z_precision_roads.py
@@ -1,13 +1,12 @@
 ###########################
-## beginnings of road zcheck...
-## Almost exact copy of zcheck_byg - merge??
+## road zcheck... Check delta for overlaps along roads
 #########################
 import sys,os
 import numpy as np
 from thatsDEM import report
 import thatsDEM.dhmqc_constants as const
 import zcheck_base
-DEBUG="-debug" in sys.argv
+from utils.osutils import ArgumentParser
 
 #SOME GLOBALS WHICH SHOULD BE PLACED IN A CONSTANTS MODULE
 
@@ -19,13 +18,28 @@ buffer_dist=2.0
 
 cut_to=const.terrain
 
+progname=os.path.basename(__file__)
+
+#Argument handling - if module has a parser attributte it will be used to check arguments in wrapper script.
+#a simple subclass of argparse,ArgumentParser which raises an exception in stead of using sys.exit if supplied with bad arguments...
+parser=ArgumentParser(description="",prog=progname)
+parser.add_argument("-use_local",action="store_true",help="Force use of local database for reporting.")
+#add some arguments below
+parser.add_argument("-class",dest="cut_to",type=int,default=cut_to,help="Inspect points of this class - defaults to 'terrain'")
+parser.add_argument("las_file",help="input 1km las tile.")
+parser.add_argument("ref_file",help="input road reference data.")
+
+
+def usage():
+	parser.print_help()
 
 def main(args):
-	lasname=args[1]
-	roadname=args[2]
-	use_local="-use_local" in args
-	reporter=report.ReportZcheckRoad(use_local)
-	done=zcheck_base.zcheck_base(lasname,roadname,angle_tolerance,xy_tolerance,z_tolerance,cut_to,reporter,buffer_dist=buffer_dist)
+	pargs=parser.parse_args(args[1:])
+	lasname=pargs.las_file
+	roadname=pargs.ref_file
+	cut=pargs.cut_to
+	reporter=report.ReportZcheckRoad(pargs.use_local)
+	done=zcheck_base.zcheck_base(lasname,roadname,angle_tolerance,xy_tolerance,z_tolerance,cut,reporter,buffer_dist=buffer_dist)
 	
 
 if __name__=="__main__":
