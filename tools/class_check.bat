3cf9cf6821c6b5cc53750027d600b382350d2bcd
Simon Kokkendorff <silyko@gmail.com>
2015-03-27 11:55:33 +0100

    more fixes and foxes

--- tools/class_check.bat
+++ tools/class_check.bat
@@ -11,11 +11,17 @@ set TOOLS_PATH=%~dp0
 set DEV_PATH=%TOOLS_PATH%..
 echo "Calling script to set reference data connections - must define REFCON, HOUSES, LAKES and ROADS"
 call reflayers.bat
+if "%REFCON%"=="" (
+   echo "REFCON must be defined"
+   exit /B
+)
+
 
 python %DEV_PATH%\qc_wrap.py -testname spike_check -schema %SCHEMA% -targs "-zlim 0.25" -tiles %TILE_DB% -runid %RUNID%
 python %DEV_PATH%\qc_wrap.py -testname count_classes -schema %SCHEMA% -tiles %TILE_DB% -runid %RUNID%
 python %DEV_PATH%\qc_wrap.py -testname classification_check -schema %SCHEMA% -tiles %TILE_DB% -runid %RUNID% -refcon %REFCON% -targs "-type building -layersql %HOUSES%"
 python %DEV_PATH%\qc_wrap.py -testname classification_check -schema %SCHEMA% -tiles %TILE_DB% -runid %RUNID% -refcon %REFCON% -targs "-type lake -layersql %LAKES%"
-python %DEV_PATH%\qc_wrap.py  templates\args_delta_roads.py -schema %SCHEMA% -tiles %TILE_DB% -runid %RUNID% -refcon %REFCON% 
+python %DEV_PATH%\qc_wrap.py -testname road_delta_check -schema %SCHEMA% -tiles %TILE_DB% -runid %RUNID% -refcon %REFCON%    -targs "-layersql %ROADS% -zlim 0.5"
 python %DEV_PATH%\qc_wrap.py -testname classification_check -schema %SCHEMA% -tiles %TILE_DB% -runid %RUNID% -refcon %REFCON% -targs "-layersql %HOUSES% -below_poly -toE" 
 python %DEV_PATH%\qc_wrap.py -testname las2polygons -schema %SCHEMA% -tiles %TILE_DB% -runid %RUNID% 
+python %DEV_PATH%\qc_wrap.py -testname wobbly_water -schema %SCHEMA% -tiles %TILE_DB% -runid %RUNID% 
