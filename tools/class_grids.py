61b23edf3d4b02c519388814e3b991e65c3b6982
thorbotnik <devnull@localhost>
2015-08-04 12:53:00 +0000

    changes to linux

--- tools/class_grids.py
+++ tools/class_grids.py
@@ -23,25 +23,27 @@ for folder in ["class_grids","dems","diff","hillshade_dtm","hillshade_dsm"]:
     if not os.path.exists(folder):
         os.mkdir(folder)
 
-rc=subprocess.call(["python",qc_wrap,"-testname","class_grid","-targs", "class_grids -cs 1","-tiles",pargs.tile_index],shell=True)
+rc=subprocess.call('python '+qc_wrap+' -testname class_grid -targs "class_grids -cs 1" -tiles '+pargs.tile_index,shell=True)
 print rc
+sys.exit(1)
 subprocess.call("gdalbuildvrt class_grid.vrt class_grids/*.tif",shell=True)
 subprocess.call("gdaladdo -ro --config COMPRESS_OVERVIEW LZW class_grid.vrt 2 4 8 16",shell=True)
 if pargs.index_2007 is not None and os.path.exists(pargs.index_2007):
-    rc=subprocess.call(["python",qc_wrap,"-testname","pointcloud_diff","-targs","-cs 4.0 -class 5 -toE -outdir diff","-tiles",pargs.tile_index,"-reftiles",pargs.index_2007],shell=True)
+    rc=subprocess.call('python '+qc_wrap+' -testname pointcloud_diff -targs "-cs 4.0 -class 5 -toE -outdir diff" -tiles '+pargs.tile_index+' -reftiles '+pargs.index_2007,shell=True)
     subprocess.call("gdalbuildvrt diff.vrt diff/*.tif",shell=True)
     subprocess.call("gdaladdo -ro --config COMPRESS_OVERVIEW LZW diff.vrt  2 4 8 16",shell=True)
-subprocess.call(["python",qc_wrap,"-testname","dem_gen_new","-tiles",pargs.tile_index,"-targs",pargs.tile_index+" dems -dtm -dsm -nowarp -overwrite"],shell=True)  
+subprocess.call('python '+qc_wrap+' -testname dem_gen_new -tiles '+pargs.tile_index+' -targs "'+pargs.tile_index+' dems -dtm -dsm -nowarp -overwrite"',shell=True)  
 if os.path.exists("dtm.sqlite"):
     os.remove("dtm_sqlite")
 if os.path.exists("dsm.sqlite"):
     os.remove("dsm.sqlite")
 
-subprocess.call(["python",tile_coverage,"create","dems","tif","dtm.sqlite","--fpat","dtm"],shell=True)
-subprocess.call(["python",tile_coverage,"create","dems","tif","dsm.sqlite","--fpat","dsm"],shell=True)
-subprocess.call(["python",qc_wrap,"-testname","hillshade","-tiles","dtm.sqlite","-targs","hillshade_dtm -tiledb dtm.sqlite"],shell=True)
-subprocess.call(["python",qc_wrap,"-testname","hillshade","-tiles","dsm.sqlite","-targs","hillshade_dsm -tiledb dsm.sqlite"],shell=True)
+subprocess.call('python '+tile_coverage+' create dems tif dtm.sqlite --fpat dtm',shell=True)
+subprocess.call('python '+tile_coverage+' create dems tif dsm.sqlite --fpat dsm',shell=True)
+subprocess.call('python '+qc_wrap+' -testname hillshade -tiles dtm.sqlite -targs "hillshade_dtm -tiledb dtm.sqlite"',shell=True)
+subprocess.call('python '+qc_wrap+' -testname hillshade -tiles dsm.sqlite -targs "hillshade_dsm -tiledb dsm.sqlite"',shell=True)
 subprocess.call("gdalbuildvrt dtm_shade.vrt hillshade_dtm/*.tif",shell=True)
 subprocess.call("gdalbuildvrt dsm_shade.vrt  hillshade_dsm/*.tif",shell=True)
 subprocess.call("gdaladdo -ro --config COMPRESS_OVERVIEW LZW -r gauss dtm_shade.vrt 4 8 16 32",shell=True)
 subprocess.call("gdaladdo -ro --config COMPRESS_OVERVIEW LZW -r gauss dsm_shade.vrt 4 8 16 32",shell=True)
+
