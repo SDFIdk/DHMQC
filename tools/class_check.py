806a7e728155349de9f2cc8c5344bd7e48bd5a37
Kristian Evers <kristianevers@gmail.com>
2015-11-05 11:12:33 +0100

    Added parameter to specify number of concurrent processses (-mp)

--- tools/class_check.py
+++ tools/class_check.py
@@ -3,21 +3,25 @@ import argparse
 import subprocess
 import sqlite3
 import time
+import multiprocessing
 
 import reflayers as rl
 
 start_time = time.time()
 
-DEV_PATH=os.path.realpath(os.path.join(os.path.dirname(__file__),".."))
-qc_wrap=os.path.join(DEV_PATH,"qc_wrap.py")
-tile_coverage=os.path.join(DEV_PATH,"tile_coverage.py")
-
 parser=argparse.ArgumentParser(description="Python wrapper for class checks")
 parser.add_argument("tile_index",help="Path to tile index file.")
 parser.add_argument("schema",help="database schema to report to.")
 parser.add_argument("-runid",default=1, type=int, help="Run id. (defaults to 1")
+parser.add_argument("-mp", default=multiprocessing.cpu_count(), help='Specify maximal number of processes to spawn (defaults to number of kernels).')
 pargs=parser.parse_args()
 pargs.tile_index=os.path.abspath(pargs.tile_index).replace("\\","/")
+
+mp = min(pargs.mp, multiprocessing.cpu_count())
+
+DEV_PATH=os.path.realpath(os.path.join(os.path.dirname(__file__),".."))
+qc_wrap=os.path.join(DEV_PATH,"qc_wrap.py -mp %s" % mp)
+tile_coverage=os.path.join(DEV_PATH,"tile_coverage.py")
 
 RUNID=str(pargs.runid)
 
@@ -25,22 +29,22 @@ RUNID=str(pargs.runid)
 if not os.path.exists(pargs.tile_index):
     print("Tile index must exist!")
     sys.exit(1)
-	
+
 mconn =sqlite3.connect(pargs.tile_index)
 mc=mconn.cursor()
 mc.execute("""select count(*) from coverage""")
 amount_of_files=mc.fetchone()[0]
-mconn.close()	
-	
+mconn.close()
+
 exestrings=[]
-exestrings.append( """python %s/qc_wrap.py -testname spike_check -schema %s -targs "-zlim 0.25" -tiles %s -runid %s""" %(DEV_PATH, pargs.schema, pargs.tile_index, RUNID))
-exestrings.append("""python %s/qc_wrap.py -testname count_classes -schema %s -tiles %s -runid %s""" %(DEV_PATH,pargs.schema, pargs.tile_index, RUNID))
-exestrings.append("""python %s/qc_wrap.py -testname classification_check -schema %s -tiles %s -runid %s -refcon "%s" -targs "-type building -layersql '%s'"  """ %(DEV_PATH, pargs.schema, pargs.tile_index, RUNID, rl.REFCON, rl.HOUSES))
-exestrings.append("""python %s/qc_wrap.py -testname classification_check -schema %s -tiles %s -runid %s -refcon "%s" -targs "-type lake -layersql '%s'"  """ %(DEV_PATH, pargs.schema, pargs.tile_index, RUNID, rl.REFCON, rl.LAKES))
-exestrings.append("""python %s/qc_wrap.py -testname road_delta_check -schema %s -tiles %s -runid %s -refcon "%s" -targs "-layersql '%s' -zlim 0.5" """%(DEV_PATH, pargs.schema, pargs.tile_index, RUNID, rl.REFCON,rl.ROADS))
-exestrings.append("""python %s/qc_wrap.py -testname classification_check -schema %s -tiles %s -runid %s -refcon "%s" -targs "-layersql '%s' -below_poly -toE" """%(DEV_PATH, pargs.schema, pargs.tile_index, RUNID, rl.REFCON,rl.HOUSES))
-exestrings.append("""python %s/qc_wrap.py -testname las2polygons -schema %s -tiles %s -runid %s"""%(DEV_PATH, pargs.schema, pargs.tile_index, RUNID))
-exestrings.append("""python %s/qc_wrap.py -testname wobbly_water -schema %s -tiles %s -runid %s"""%(DEV_PATH, pargs.schema, pargs.tile_index, RUNID))
+exestrings.append("""python %s -testname spike_check -schema %s -targs "-zlim 0.25" -tiles %s -runid %s""" % (qc_wrap, pargs.schema, pargs.tile_index, RUNID))
+exestrings.append("""python %s -testname count_classes -schema %s -tiles %s -runid %s""" % (qc_wrap,pargs.schema, pargs.tile_index, RUNID))
+exestrings.append("""python %s -testname classification_check -schema %s -tiles %s -runid %s -refcon "%s" -targs "-type building -layersql '%s'"  """ % (qc_wrap, pargs.schema, pargs.tile_index, RUNID, rl.REFCON, rl.HOUSES))
+exestrings.append("""python %s -testname classification_check -schema %s -tiles %s -runid %s -refcon "%s" -targs "-type lake -layersql '%s'"  """ % (qc_wrap, pargs.schema, pargs.tile_index, RUNID, rl.REFCON, rl.LAKES))
+exestrings.append("""python %s -testname road_delta_check -schema %s -tiles %s -runid %s -refcon "%s" -targs "-layersql '%s' -zlim 0.5" """ % (qc_wrap, pargs.schema, pargs.tile_index, RUNID, rl.REFCON,rl.ROADS))
+exestrings.append("""python %s -testname classification_check -schema %s -tiles %s -runid %s -refcon "%s" -targs "-layersql '%s' -below_poly -toE" """ % (qc_wrap, pargs.schema, pargs.tile_index, RUNID, rl.REFCON,rl.HOUSES))
+exestrings.append("""python %s -testname las2polygons -schema %s -tiles %s -runid %s""" % (qc_wrap, pargs.schema, pargs.tile_index, RUNID))
+exestrings.append("""python %s -testname wobbly_water -schema %s -tiles %s -runid %s""" % (qc_wrap, pargs.schema, pargs.tile_index, RUNID))
 
 
 for exestring in exestrings:
@@ -48,7 +52,7 @@ for exestring in exestrings:
 	print exestring
 	print "-------------------"
 	subprocess.call(exestring,shell=True)
-	
+
 
 end_time = time.time()
 
--- tools/class_grids.py
+++ tools/class_grids.py
@@ -1,79 +1,89 @@
-import os,sys
+import os
+import sys
 import argparse
 import subprocess
 import sqlite3
 import time
+import multiprocessing
 
 start_time = time.time()
 
-DEV_PATH=os.path.realpath(os.path.join(os.path.dirname(__file__),".."))
-qc_wrap=os.path.join(DEV_PATH,"qc_wrap.py")
-tile_coverage=os.path.join(DEV_PATH,"tile_coverage.py")
+parser = argparse.ArgumentParser(description="Python wrapper for class grids")
+parser.add_argument("tile_index", help="Path to tile index file.")
+parser.add_argument("outdir", help="Output folder.")
+parser.add_argument("-index_2007", help="Path to index of 2007 las files.")
+parser.add_argument("-only_dems", action="store_true",help="Only do dems and hillshade.")
+parser.add_argument('-mp', default=multiprocessing.cpu_count(), help='Specify maximal number of processes to spawn (defaults to number of kernels).')
 
-parser=argparse.ArgumentParser(description="Python wrapper for class grids")
-parser.add_argument("tile_index",help="Path to tile index file.")
-parser.add_argument("outdir",help="Output folder.")
-parser.add_argument("-index_2007",help="Path to index of 2007 las files.")
-parser.add_argument("-only_dems",action="store_true",help="Only do dems and hillshade.")
-pargs=parser.parse_args()
-pargs.tile_index=os.path.abspath(pargs.tile_index).replace("\\","/")
+pargs = parser.parse_args()
+
+mp = min(pargs.mp, multiprocessing.cpu_count())
+
+DEV_PATH = os.path.realpath(os.path.join(os.path.dirname(__file__), ".."))
+qc_wrap = os.path.join(DEV_PATH, "qc_wrap.py -mp %s" % mp)
+tile_coverage = os.path.join(DEV_PATH, "tile_coverage.py")
+
+pargs.tile_index = os.path.abspath(pargs.tile_index).replace("\\", "/")
 if pargs.index_2007 is not None:
-    pargs.index_2007=os.path.abspath(pargs.index_2007).replace("\\","/")
+    pargs.index_2007 = os.path.abspath(pargs.index_2007).replace("\\", "/")
 if not os.path.exists(pargs.tile_index):
     print("Tile index must exist!")
     sys.exit(1)
 
-mconn =sqlite3.connect(pargs.tile_index)
-mc=mconn.cursor()
+mconn = sqlite3.connect(pargs.tile_index)
+mc = mconn.cursor()
 mc.execute("""select count(*) from coverage""")
-amount_of_files=mc.fetchone()[0]
+amount_of_files = mc.fetchone()[0]
 mconn.close()
-    
+
 
 if not os.path.exists(pargs.outdir):
     os.makedirs(pargs.outdir)
 os.chdir(pargs.outdir)
-for folder in ["class_grids","dems","diff","hillshade_dtm","hillshade_dsm"]:
+for folder in ["class_grids", "dems", "diff", "hillshade_dtm", "hillshade_dsm"]:
     if not os.path.exists(folder):
         os.mkdir(folder)
 
 if not pargs.only_dems:
-    rc=subprocess.call('python '+qc_wrap+' -testname class_grid -targs "class_grids -cs 1" -tiles '+pargs.tile_index,shell=True)
+    call = 'python %s -testname class_grid -targs "class_grids -cs 1" -tiles %s' % (qc_wrap, pargs.tile_index)
+    rc = subprocess.call(call, shell=True)
     print rc
-    subprocess.call("gdalbuildvrt class_grid.vrt class_grids/*.tif",shell=True)
-    subprocess.call("gdaladdo -ro --config COMPRESS_OVERVIEW LZW class_grid.vrt 2 4 8 16",shell=True)
+    subprocess.call("gdalbuildvrt class_grid.vrt class_grids/*.tif", shell=True)
+    subprocess.call("gdaladdo -ro --config COMPRESS_OVERVIEW LZW class_grid.vrt 2 4 8 16", shell=True)
     if pargs.index_2007 is not None and os.path.exists(pargs.index_2007):
-        rc=subprocess.call('python '+qc_wrap+' -testname pointcloud_diff -targs "-cs 4.0 -class 5 -toE -outdir diff" -tiles '+pargs.tile_index+' -reftiles '+pargs.index_2007,shell=True)
-        subprocess.call("gdalbuildvrt diff.vrt diff/*.tif",shell=True)
-        subprocess.call("gdaladdo -ro --config COMPRESS_OVERVIEW LZW diff.vrt  2 4 8 16",shell=True)
+        call = 'python %s -testname pointcloud_diff -targs "-cs 4.0 -class 5 -toE -outdir diff" -tiles %s -reftiles %s' % (qc_wrap, pargs.tile_index, pargs.index_2007)
+        rc = subprocess.call(call, shell=True)
+        subprocess.call("gdalbuildvrt diff.vrt diff/*.tif", shell=True)
+        subprocess.call("gdaladdo -ro --config COMPRESS_OVERVIEW LZW diff.vrt  2 4 8 16", shell=True)
 
-subprocess.call('python '+qc_wrap+' -testname dem_gen_new -tiles '+pargs.tile_index+' -targs "'+pargs.tile_index+' dems -dtm -dsm -nowarp -overwrite"',shell=True)  
-    
+subprocess.call('python ' + qc_wrap +' -testname dem_gen_new -tiles ' + pargs.tile_index +' -targs "' + pargs.tile_index + ' dems -dtm -dsm -nowarp -overwrite"', shell=True)
+
 if os.path.exists("dtm.sqlite"):
     os.remove("dtm.sqlite")
 if os.path.exists("dsm.sqlite"):
     os.remove("dsm.sqlite")
 
-subprocess.call('python '+tile_coverage+' create dems tif dtm.sqlite --fpat dtm',shell=True)
-subprocess.call('python '+tile_coverage+' create dems tif dsm.sqlite --fpat dsm',shell=True)
-subprocess.call('python '+qc_wrap+' -testname hillshade -tiles dtm.sqlite -targs "hillshade_dtm -tiledb dtm.sqlite"',shell=True)
-subprocess.call('python '+qc_wrap+' -testname hillshade -tiles dsm.sqlite -targs "hillshade_dsm -tiledb dsm.sqlite"',shell=True)
-subprocess.call("gdalbuildvrt dtm_shade.vrt hillshade_dtm/*.tif",shell=True)
-subprocess.call("gdalbuildvrt dsm_shade.vrt  hillshade_dsm/*.tif",shell=True)
-subprocess.call("gdaladdo -ro --config COMPRESS_OVERVIEW LZW -r gauss dtm_shade.vrt 4 8 16 32",shell=True)
-subprocess.call("gdaladdo -ro --config COMPRESS_OVERVIEW LZW -r gauss dsm_shade.vrt 4 8 16 32",shell=True)
+subprocess.call('python ' + tile_coverage + ' create dems tif dtm.sqlite --fpat dtm', shell=True)
+subprocess.call('python ' + tile_coverage + ' create dems tif dsm.sqlite --fpat dsm', shell=True)
+call = 'python %s -testname hillshade -tiles dtm.sqlite -targs "hillshade_dtm -tiledb dtm.sqlite"' % qc_wrap
+subprocess.call(call, shell=True)
+call = 'python %s -testname hillshade -tiles dsm.sqlite -targs "hillshade_dsm -tiledb dsm.sqlite"' % qc_wrap
+subprocess.call(call, shell=True)
+subprocess.call("gdalbuildvrt dtm_shade.vrt hillshade_dtm/*.tif", shell=True)
+subprocess.call("gdalbuildvrt dsm_shade.vrt  hillshade_dsm/*.tif", shell=True)
+subprocess.call("gdaladdo -ro --config COMPRESS_OVERVIEW LZW -r gauss dtm_shade.vrt 4 8 16 32", shell=True)
+subprocess.call("gdaladdo -ro --config COMPRESS_OVERVIEW LZW -r gauss dsm_shade.vrt 4 8 16 32", shell=True)
 
 end_time = time.time()
 
-total_time=end_time-start_time
+total_time = end_time - start_time
 
 print " "
 print " "
 print "------------------------------------------"
 print "Summary: "
-print "  Files processed:      %d"%(amount_of_files)
-print "  Total execution time: %.1f min" %(total_time/60)
-print "  Average:              %.1f files/min" %(amount_of_files/(total_time/60))
+print "  Files processed:      %d" % (amount_of_files)
+print "  Total execution time: %.1f min" % (total_time / 60)
+print "  Average:              %.1f files/min" % (amount_of_files / (total_time / 60))
 print "------------------------------------------"
 print " "
- 
