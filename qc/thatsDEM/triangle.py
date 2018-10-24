07f19264069071a213303e779604c26230ced0a6
Peter Limkilde Svendsen <pelis@sdfe.dk>
2018-10-24 14:35:56 +0200

    Update copyright notices

--- qc/thatsDEM/triangle.py
+++ qc/thatsDEM/triangle.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2015, Danish Geodata Agency <gst@gst.dk>
+# Copyright (c) 2018, Danish Agency for Data Supply and Efficiency <sdfe@sdfe.dk>
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
--- src/build/build.py
+++ src/build/build.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2015-2016, Danish Geodata Agency <gst@gst.dk>
-# Copyright (c) 2016, Danish Agency for Data Supply and Efficiency <sdfe@sdfe.dk>
+# Copyright (c) 2016-2018, Danish Agency for Data Supply and Efficiency <sdfe@sdfe.dk>
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
